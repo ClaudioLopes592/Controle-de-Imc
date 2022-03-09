f3d0f8ae62f2187fe7e2e4b03fff2f2108edf85d
ClaudioLopes592 <claudioglopes1@gmail.com>
2022-03-09 19:48:09 -0300

    concluindo backend

--- imc.py
+++ imc.py
@@ -1,6 +1,6 @@
+from turtle import title
 from kivymd.app import MDApp
 from kivymd.uix.boxlayout import MDBoxLayout
-from kivy.properties import StringProperty, NumericProperty
 from kivy.core.window import Window
 
 
@@ -11,12 +11,38 @@ class TelaPrincipal(MDBoxLayout):
 
     def calcular_imc(self):
         peso = self.ids['txt_peso'].text
+        peso = peso.replace(',', '.')
         altura = self.ids['txt_altura'].text
-        peso = float(peso)
-        altura = float(altura)
-        imc = peso / (altura * altura)
-        self.ids['lbl_resultado'].text = (
-            f'O IMC calculado é de {imc:.1f} kg/m²')
+        altura = altura.replace(',', '.')
+        if peso == '':
+            self.ids['lbl_resultado'].text = ('O campo peso é obrigatório')
+        elif altura == '':
+            self.ids['lbl_resultado'].text = ('O campo altura é obrigatório')
+        else:
+            self.ids['lbl_resultado'].text = ('')
+            peso = float(peso)
+            altura = float(altura)
+            imc = peso / (altura * altura)
+            self.exibir_status(imc)
+
+    def exibir_status(self, imc):
+        if imc < 18.5:
+            self.ids['lbl_resultado'].text = (
+                f'Com {imc:.1f} kg/m² sua classificação é MAGREZA OBESIDADE GRAU 0')
+        elif imc >= 18.5 and imc < 25.0:
+            self.ids['lbl_resultado'].text = (
+                f'Com {imc:.1f} kg/m² sua classificação é NORMAL OBESIDADE GRAU 0')
+        elif imc >= 25.0 and imc < 30.0:
+            self.ids['lbl_resultado'].text = (
+                f'Com {imc:.1f} kg/m² sua classificação é SOBREPESO OBESIDADE GRAU I')
+        elif imc >= 30.0 and imc <= 40.0:
+            self.ids['lbl_resultado'].text = (
+                f'Com {imc:.1f} kg/m² sua classificação é OBESIDADE GRAU II')
+        elif imc > 40.0:
+            self.ids['lbl_resultado'].text = (
+                f'Com {imc:.1f} kg/m² sua classificação é OBESIDADE GRAU III')
+        else:
+            self.ids['lbl_resultado'].text = ''
 
 
 class Imc(MDApp):
